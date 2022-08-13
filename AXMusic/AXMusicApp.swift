106c6afa77b6ed4f90161b17d4d656e67611dd3d
Ashwin <ashwonixer123@gmail.com>
2022-08-13 08:14:27 -0400

    Initial Commit

---
+++ AXMusic/AXMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AXMusicApp.swift
+//  AXMusic
+//
+//  Created by Ashwin Paudel on 2022-08-13.
+//
+
+import SwiftUI
+
+@main
+struct AXMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AXMusic/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AXMusic
+//
+//  Created by Ashwin Paudel on 2022-08-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
